de41ac37fd4e6b1bbb7c89e697121f4a76c16935
kevim <k.v.n.such@gmail.com>
2022-09-30 16:52:10 -0300

    Adicionando thymeleaf e outras coisas

--- src/main/java/com/example/filexplorer/controller/FileEntryController.java
+++ src/main/java/com/example/filexplorer/controller/FileEntryController.java
@@ -14,45 +14,65 @@ import java.util.List;
 
 @Controller
 public class FileEntryController {
-	final private FileRegisterService fileRegisterService;
+    final private FileRegisterService fileRegisterService;
 
-	public FileEntryController(FileRegisterService fileRegisterService) {
-		this.fileRegisterService = fileRegisterService;
-	}
+    public FileEntryController(FileRegisterService fileRegisterService) {
+        this.fileRegisterService = fileRegisterService;
+    }
 
-	@GetMapping("/fileregistry/{fileuuid}")
-	public String greeting(@PathVariable(name = "fileuuid") String fileuuid, Model model) {
-		final FileEntryDto entryDto = fileRegisterService.listEntrys(fileuuid).get(0);
-		if(entryDto!=null){
-			List<BreadDto> breadlist = new ArrayList<>();
-			breadlist.add(new BreadDto("List", "/fileregistry/list"));
-			breadlist.add(new BreadDto(entryDto.getFileuuid(), "/fileregistry/"+entryDto.getFileuuid(), true));
-			model.addAttribute("breadlist", breadlist);
-			model.addAttribute("fileentry", entryDto);
-		}
-		return "fileentry/get-template";
-	}
+    final static BreadDto BREAD_HOME = new BreadDto("Home", "/fileregistry/");
 
-	@GetMapping("/fragments/header")
-	public String getHeader(Model model) {
-		return "fragments/header";
-	}
+    @GetMapping("/fileregistry/{fileuuid}")
+    public String greeting(@PathVariable(name = "fileuuid") String fileuuid, Model model) {
+        final FileEntryDto entryDto = fileRegisterService.listEntrys(fileuuid).get(0);
+        if (entryDto != null) {
+            setBreacumb(model,
+                    BREAD_HOME,
+                    new BreadDto("List", "/fileregistry/list"),
+                    new BreadDto(entryDto.getFileuuid(), "/fileregistry/" + entryDto.getFileuuid(), true)
+            );
+            model.addAttribute("fileentry", entryDto);
+        }
+        return "fileentry/get-template";
+    }
 
-	@GetMapping({"/fileregistry/list", "/fileregistry/"})
-	public String list(Model model) {
-		List<BreadDto> breadlist = new ArrayList<>();
-		breadlist.add(new BreadDto("List", "/fileregistry/list"));
-		model.addAttribute("breadlist", breadlist);
-		model.addAttribute("title", "List");
-		List<FileEntryDto> entrys = fileRegisterService.listEntrys();
-		if(entrys==null || entrys.isEmpty()){
-			model.addAttribute("fileentrylist", new ArrayList<>());
+    private void setBreacumb(Model model, BreadDto... breads) {
+        List<BreadDto> breadlist = new ArrayList<>();
+        for (BreadDto bread : breads) {
+            breadlist.add(bread);
+        }
+        model.addAttribute("breadlist", breadlist);
+    }
 
-			return "fileentry/list-template";
-		}
-		model.addAttribute("fileentrylist", entrys);
-		return "fileentry/list-template";
-	}
+    @GetMapping("/fragments/header")
+    public String getHome(Model model) {
+        return "fragments/header";
+    }
+
+    @GetMapping("/fileregistry/")
+    public String getHeader(Model model) {
+        setBreacumb(model,
+                new BreadDto(BREAD_HOME.getName(), BREAD_HOME.getLink(), true)
+        );
+        return "fileentry/home-template";
+    }
+
+    @GetMapping({"/fileregistry/list"})
+    public String list(Model model) {
+        setBreacumb(model,
+                BREAD_HOME,
+                new BreadDto("List", "/fileregistry/list")
+        );
+        model.addAttribute("title", "List");
+        List<FileEntryDto> entrys = fileRegisterService.listEntrys();
+        if (entrys == null || entrys.isEmpty()) {
+            model.addAttribute("fileentrylist", new ArrayList<>());
+
+            return "fileentry/list-template";
+        }
+        model.addAttribute("fileentrylist", entrys);
+        return "fileentry/list-template";
+    }
 
 
 }
